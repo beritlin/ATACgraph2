3b0746a2929face178fa8169900e9e38c854d697
Pei-Yu Lin <39916474+beritlin@users.noreply.github.com>
2023-12-19 13:08:23 +0800

    idr

--- website/server.r
+++ website/server.r
@@ -5,16 +5,18 @@ library(plotly)
 library(ggpointdensity)
 library(shinycssloaders)
 library(DT)
+library(ggplot2)
+library(shinyWidgets)
+library(dplyr)
 
-# library(processx)
- options(shiny.maxRequestSize = 30*1024^2)
+
+
+options(shiny.maxRequestSize = 30 * 1024^2)
 
 shinyServer(function(input, output, session) {
-
-
   output$scatter_plot <- renderPlotly(NULL)
 
-#### gene expression
+  #### gene expression
   observeEvent(input$generate_button, {
     # Check if both files are uploaded
     if (is.null(input$gene_file) || is.null(input$atac_file)) {
@@ -23,7 +25,7 @@ shinyServer(function(input, output, session) {
 
     # Read gene expression file
     gene_raw <- read.table(input$gene_file$datapath, header = FALSE)
-    gene_raw <- gene_raw[order(gene_raw[, 'V2']), ]
+    gene_raw <- gene_raw[order(gene_raw[, "V2"]), ]
     gene_raw$rank <- seq(1, dim(gene_raw)[1])
 
     # Read ATAC-seq abundance file
@@ -38,35 +40,46 @@ shinyServer(function(input, output, session) {
     if (input$plot_type == "scatter") {
       output$scatter_plot <- renderPlotly({
         ggplotly(ggplot(aes(y = atac, x = rna), data = data_raw) +
-                   theme_minimal() +
-                   geom_pointdensity() +
-                   geom_smooth(method = "lm", formula = y ~ x, se = FALSE, colour = "pink2")) %>%
-          layout(yaxis = list(title = "ATAC-seq Abundance"),
-                 xaxis = list(title = "Gene expression"),
-                 showlegend = FALSE,
-                 annotations = list(x = max(data_raw$rna),
-                                    y = max(data_raw$atac),
-                                    text = paste0('R', " = ", round(cor(data_raw$rna, data_raw$atac, method = 'spearman'), 3),
-                                                  "\n", ", p = ", cor.test(data_raw$rna, data_raw$atac, method = 'spearman')$p.value),
-                                    showarrow = FALSE
-                 )
-        )%>% toWebGL()
+          theme_minimal() +
+          geom_pointdensity() +
+          geom_smooth(method = "lm", formula = y ~ x, se = FALSE, colour = "pink2")) %>%
+          layout(
+            yaxis = list(title = "<b>ATAC-seq Abundance</b>"),
+            xaxis = list(title = "<b>Gene expression</b>"),
+            showlegend = FALSE,
+            annotations = list(
+              x = max(data_raw$rna),
+              y = max(data_raw$atac),
+              text = paste0(
+                "R", " = ", round(cor(data_raw$rna, data_raw$atac, method = "spearman"), 3),
+                "\n", ", p = ", cor.test(data_raw$rna, data_raw$atac, method = "spearman")$p.value
+              ),
+              showarrow = FALSE
+            )
+          ) %>%
+          toWebGL()
       })
     } else {
       output$scatter_plot <- renderPlotly({
         ggplotly(ggplot(aes(y = atac, x = rank), data = data_raw) +
-                   geom_point(alpha = 0.6, colour = "lightgrey") +
-                   geom_smooth(method = "lm", formula = y ~ x, se = FALSE, colour = "black") +
-                   theme_minimal()) %>%
-          layout(yaxis = list(title = "ATAC-seq Abundance"),
-                 xaxis = list(title = "Rank of gene expression"),
-                 showlegend = FALSE,
-                 annotations = list(x = max(data_raw$rank),
-                                    y = max(data_raw$atac),
-                                    text = paste0('R', " = ", round(cor(data_raw$rank, data_raw$atac, method = 'spearman'), 3),
-                                                  "\n", ", p = ", cor.test(data_raw$rank, data_raw$atac, method = 'spearman')$p.value),
-                                    showarrow = FALSE
-                 ))%>% toWebGL()
+          geom_point(alpha = 0.6, colour = "lightgrey") +
+          geom_smooth(method = "lm", formula = y ~ x, se = FALSE, colour = "black") +
+          theme_minimal()) %>%
+          layout(
+            yaxis = list(title = "<b>ATAC-seq Abundance</b>"),
+            xaxis = list(title = "<b>Rank of gene expression</b>"),
+            showlegend = FALSE,
+            annotations = list(
+              x = max(data_raw$rank),
+              y = max(data_raw$atac),
+              text = paste0(
+                "R", " = ", round(cor(data_raw$rank, data_raw$atac, method = "spearman"), 3),
+                "\n", ", p = ", cor.test(data_raw$rank, data_raw$atac, method = "spearman")$p.value
+              ),
+              showarrow = FALSE
+            )
+          ) %>%
+          toWebGL()
       })
     }
   })
@@ -81,15 +94,14 @@ shinyServer(function(input, output, session) {
 
 
 
-#### idr
+  #### idr
 
   output$idr_plot_rank <- renderPlotly(NULL)
   output$idr_plot_sig <- renderPlotly(NULL)
   output$idr_para_int <- renderText(NULL)
 
   observeEvent(input$generate_button_idr, {
-
-  output$idr_para_int <- renderText(paste("<b>","Result","</b>"))
+    output$idr_para_int <- renderText(paste("<b>", "Result", "</b>"))
 
 
     # Check if both files are uploaded
@@ -98,95 +110,127 @@ shinyServer(function(input, output, session) {
     }
 
     # calculating idr
-    system(paste0("/work1/home/peiyu/anaconda3/envs/atac/bin/idr --samples ", input$atac_file1_idr$datapath," ",input$atac_file2_idr$datapath, "  --input-file-type narrowPeak --rank p.value --output-file /work1/home/peiyu/2023_atac/tmp/idr.txt --log-output-file /work1/home/peiyu/2023_atac/tmp/tmp.log"))
+    system(paste0("/work1/home/peiyu/anaconda3/envs/atac/bin/idr --samples ", input$atac_file1_idr$datapath, " ", input$atac_file2_idr$datapath, "  --input-file-type narrowPeak --rank p.value --output-file /work1/home/peiyu/2023_atac/tmp/idr.txt --log-output-file /work1/home/peiyu/2023_atac/tmp/tmp.log"))
 
 
     # output idr
-    atac_idr_log <- read.csv("/work1/home/peiyu/2023_atac/tmp/tmp.log", header = FALSE, sep=" ") 
-    output$idr_value <- renderText(paste0("Number of peaks passing IDR cutoff of 0.05: ",atac_idr_log[4,10],"<b>", " ",atac_idr_log[4,11],"</b>"))
-    output$idr_para <- renderText(paste0("Mu=", unlist(strsplit(atac_idr_log[2,4], "\\["))[2], ", Sigma=", atac_idr_log[2,5], ", Rho=", atac_idr_log[2,6], ", Mixture params=", unlist(strsplit(atac_idr_log[2,7], "\\]"))))
+    atac_idr_log <- read.csv("/work1/home/peiyu/2023_atac/tmp/tmp.log", header = FALSE, sep = " ")
+    output$idr_value <- renderText(paste0("Number of peaks passing IDR cutoff of 0.05: ", atac_idr_log[4, 10], "<b>", " ", atac_idr_log[4, 11], "</b>"))
+    output$idr_para <- renderText(paste0("Mu=", unlist(strsplit(atac_idr_log[2, 4], "\\["))[2], ", Sigma=", atac_idr_log[2, 5], ", Rho=", atac_idr_log[2, 6], ", Mixture params=", unlist(strsplit(atac_idr_log[2, 7], "\\]"))))
 
     # plot idr
-    atac_idr_raw <- read.table("/work1/home/peiyu/2023_atac/tmp/idr.txt",header=F)
-    colnames(atac_idr_raw)[1:12] <- c("Chr","Start","End","Name","Score","Strand","Signal","pValue","qValue","Summit","LocalIDR","GlobalIDR")
+    atac_idr_raw <- read.table("/work1/home/peiyu/2023_atac/tmp/idr.txt", header = F)
+    colnames(atac_idr_raw)[1:12] <- c("Chr", "Start", "End", "Name", "Score", "Strand", "Signal", "pValue", "qValue", "Summit", "LocalIDR", "GlobalIDR")
     atac_idr_raw$idr <- ifelse(atac_idr_raw$Score > 540, "TRUE", "FALSE")
 
-    atac_idr_raw <- atac_idr_raw[order(atac_idr_raw$V15),]
+    atac_idr_raw <- atac_idr_raw[order(atac_idr_raw$V15), ]
     atac_idr_raw$rank1 <- seq(1, nrow(atac_idr_raw), 1)
-    atac_idr_raw <- atac_idr_raw[order(atac_idr_raw$V19),]
+    atac_idr_raw <- atac_idr_raw[order(atac_idr_raw$V19), ]
     atac_idr_raw$rank2 <- seq(1, nrow(atac_idr_raw), 1)
 
     atac_idr_raw$box1 <- as.factor(as.numeric(cut_number(atac_idr_raw$rank1, 20)))
     atac_idr_raw$box2 <- as.factor(as.numeric(cut_number(atac_idr_raw$rank2, 20)))
 
-    # plot 
+    # plot
     output$idr_plot_rank <- renderPlotly({
-      ggplotly(ggplot(aes(x=log(V15),y=log(V19),color=idr),data=atac_idr_raw) +
-                   theme_classic() +
-                   geom_point(size=1,alpha=.5)+
-                  scale_color_manual(values=c('indianred','gray23'))) %>%
-           layout(yaxis = list(title = "<b> Replicate 2 signal (log)<b> "),
-                  xaxis = list(title = "<b> Replicate 1 signal (log)<b> "),
-                  legend = list(title=list(text='<b> IDR >= 0.05 : </b>'),x = 0.2, y = 1.2, orientation = 'h')
-        )%>% toWebGL()
-      })
+      ggplotly(ggplot(aes(x = log(V15), y = log(V19), color = idr), data = atac_idr_raw) +
+        theme_classic() +
+        geom_point(size = 1, alpha = .5) +
+        scale_color_manual(values = c("indianred", "gray23"))) %>%
+        layout(
+          yaxis = list(title = "<b> Replicate 2 signal (log)<b> "),
+          xaxis = list(title = "<b> Replicate 1 signal (log)<b> "),
+          legend = list(title = list(text = "<b> IDR >= 0.05 : </b>"), x = 0.2, y = 1.2, orientation = "h")
+        ) %>%
+        toWebGL()
+    })
 
     output$idr_plot_sig <- renderPlotly({
-        ggplotly(ggplot(aes(x=rank1,y=rank2,color=idr),data=atac_idr_raw) +
-                   theme_classic() +
-                   geom_point(size=1,alpha=.5)+
-                  scale_color_manual(values=c('indianred','gray23'))) %>%
-           layout(yaxis = list(title = "<b> Replicate 2 rank<b> "),
-                  xaxis = list(title = "<b> Replicate 1 rank<b> "),
-                  legend = list(title=list(text='<b> IDR < 0.05 : </b>'),x = 0.2, y = 1.2, orientation = 'h')
-        )%>% toWebGL()
-      })
+      ggplotly(ggplot(aes(x = rank1, y = rank2, color = idr), data = atac_idr_raw) +
+        theme_classic() +
+        geom_point(size = 1, alpha = .5) +
+        scale_color_manual(values = c("indianred", "gray23"))) %>%
+        layout(
+          yaxis = list(title = "<b> Replicate 2 rank<b> "),
+          xaxis = list(title = "<b> Replicate 1 rank<b> "),
+          legend = list(title = list(text = "<b> IDR < 0.05 : </b>"), x = 0.2, y = 1.5, orientation = "h")
+        ) %>%
+        toWebGL()
+    })
 
-      output$idr_plot_r1 <- renderPlot(
-        ggplot(data=atac_idr_raw,aes(x=rank1,y=GlobalIDR,fill=box1)) +
-                geom_point(colour="gray65",size=1,alpha=.5)+ 
-                geom_boxplot(colour="black",outlier.shape = NA)+
-                theme_classic()+
-                scale_fill_manual(values=rep("NA",20))+
-                theme(legend.position='none',
-                      axis.title = element_text(face="bold",size=18))+
-                labs(x = 'Repliacte 1 peak rank',
-                      y = 'IDR (-log10)')
-      )
+    output$idr_plot_r1 <- renderPlot(
+      ggplot(data = atac_idr_raw, aes(x = rank1, y = GlobalIDR, fill = box1)) +
+        geom_point(colour = "gray65", size = 1, alpha = .5) +
+        geom_boxplot(colour = "black", outlier.shape = NA) +
+        theme_classic() +
+        scale_fill_manual(values = rep("NA", 20)) +
+        theme(
+          legend.position = "none",
+          axis.title = element_text(face = "bold", size = 18)
+        ) +
+        labs(
+          x = "Repliacte 1 peak rank",
+          y = "IDR (-log10)"
+        )
+    )
 
-      output$idr_plot_r2 <- renderPlot(
-        ggplot(data=atac_idr_raw,aes(x=rank2,y=GlobalIDR,fill=box2)) +
-                geom_point(colour="gray65",size=1,alpha=.5)+ 
-                geom_boxplot(colour="black",outlier.shape = NA)+
-                theme_classic()+
-                scale_fill_manual(values=rep("NA",20))+
-                theme(legend.position='none',
-                      axis.title = element_text(face="bold",size=18))+
-                labs(x = 'Repliacte 1 peak rank',
-                      y = 'IDR (-log10)')
-      )
-
-      output$idr_table <- DT::renderDT({
-          datatable(atac_idr_raw[,1:12]) 
-      })
-
-
-  # Add clear button functionality
-  observeEvent(input$clear_button_idr, {
-    # shinyjs::reset("atac_file1_idr")
-    # shinyjs::reset("atac_file2_idr")
-    output$idr_plot_r1 <- renderPlot(NULL)
-    output$idr_plot_r2 <- renderPlot(NULL)
-    output$idr_plot_sig <- renderPlotly(NULL)
-    output$idr_plot_rank <- renderPlotly(NULL)
-    output$idr_value <- renderText(NULL)
-    output$idr_para <- renderText(NULL)
-    output$idr_para_int <- renderText(NULL)
-    output$idr_table <- renderDT(NULL)
-   })
+    output$idr_plot_r2 <- renderPlot(
+      ggplot(data = atac_idr_raw, aes(x = rank2, y = GlobalIDR, fill = box2)) +
+        geom_point(colour = "gray65", size = 1, alpha = .5) +
+        geom_boxplot(colour = "black", outlier.shape = NA) +
+        theme_classic() +
+        scale_fill_manual(values = rep("NA", 20)) +
+        theme(
+          legend.position = "none",
+          axis.title = element_text(face = "bold", size = 18)
+        ) +
+        labs(
+          x = "Repliacte 1 peak rank",
+          y = "IDR (-log10)"
+        )
+    )
 
 
-}
-)
+      # Reactive value for selected dataset ----
+  datasetInput <- reactive({
+    switch(input$idr_dataset,
+           "IDR" = atac_idr_raw[, 1:12],
+           "Replicate 1" = rename(atac_idr_raw[atac_idr_raw$Score<=540, c(1,4,13:16)],c('Chr'="Chr",'Name'="Name","Start"='V13','End'="V14",'Signal'="V15",'Summit'="V16")),
+           "Replicate 2" = rename(atac_idr_raw[atac_idr_raw$Score<=540, c(1,4,17:20)],c('Chr'="Chr",'Name'="Name",'Start'="V17",'End'="V18",'Signal'="V19",'Summit'="V20")))
+  })
+
+    # Table of selected dataset ----
+  output$idr_table <-  DT::renderDT(server = FALSE,{
+     datasetInput()
+  })
+
+
+ output$idr_downloadData <-
+    downloadHandler(
+      filename = function () {
+        paste(as.data.frame(input$idr_dataset),".csv", sep = "")
+      },
+      content = function(file) {
+        write.csv(datasetInput(), file)
+      }
+    )
+
 
 })
+
+
+    # Add clear button functionality
+    observeEvent(input$clear_button_idr, {
+      # shinyjs::reset("atac_file1_idr")
+      # shinyjs::reset("atac_file2_idr")
+      output$idr_plot_r1 <- renderPlot(NULL)
+      output$idr_plot_r2 <- renderPlot(NULL)
+      output$idr_plot_sig <- renderPlotly(NULL)
+      output$idr_plot_rank <- renderPlotly(NULL)
+      output$idr_value <- renderText(NULL)
+      output$idr_para <- renderText(NULL)
+      output$idr_para_int <- renderText(NULL)
+      output$idr_table <- renderDT(NULL)
+    })
+  })
+
--- website/ui.r
+++ website/ui.r
@@ -6,94 +6,123 @@ library(shiny)
 library(shinyjs)
 library(shinydashboard)
 library(plotly)
+library(DT)
 library(ggpointdensity)
 library(shinycssloaders)
 # library(shinythemes)
 
 # Define UI
 shinyUI(
-dashboardPage(
-  skin = "black",
-  dashboardHeader(title = "ATACgraph2.0"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Home", tabName = "home", icon = icon("house")),
-      menuItem("Correlation Analyses", tabName = "geneexpress", icon = icon("chart-line")),
-      menuItem("Differentially Peaks", tabName = "dep", icon = icon("chart-bar")),
-      menuItem("Peak Distribution", tabName = "enrich", icon = icon("chart-column")),
-      menuItem("Irreproducible Discovery Rate", tabName = "idr", icon = icon("chart-scatter"))
+  dashboardPage(
+    skin = "black",
+    # header
+    dashboardHeader(title = "ATACgraph2.0"),
+    # sidebar
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Home", tabName = "home", icon = icon("house")),
+        menuItem("Correlation Analyses", tabName = "geneexpress", icon = icon("chart-line")),
+        menuItem("Differentially Peaks", tabName = "dep", icon = icon("chart-bar")),
+        menuItem("Peak Distribution", tabName = "enrich", icon = icon("chart-column")),
+        menuItem("Irreproducible Discovery Rate", tabName = "idr", icon = icon("chart-area"))
       )
-  ),
-  dashboardBody(
-    tabItems(
-      # home tab content
-      tabItem(tabName = "home",
-        h2("Widgets tab content")
-      ),
-    # First tab content
-      tabItem(tabName = "geneexpress",
-      h2(" Correlation analyses of ATAC-seqs and RNA-seqs"),
-      fluidRow(
-        box(fileInput("gene_file", "Upload Gene Expression File (TXT format)"),
-        fileInput("atac_file", "Upload ATAC-seq Abundance File (TXT format)"),
-        radioButtons("plot_type", "Choose Plot Type:",
-                    choices = list("Scatter Plot" = "scatter", "Ranked List" = "ranked"),
-                    selected = "scatter"),
-        actionButton("generate_button", "Generate Figure"),
-        actionButton("clear_button", "Clear All"),height = 500),
-
-        box(shinycssloaders::withSpinner(plotlyOutput("scatter_plot"), color = "grey"),height = 500)
-      )),
-       # Second tab content
-      tabItem(tabName = "dep",
-        h2("Widgets tab content")
-      ),
-      # Thirf tab content
-      tabItem(tabName = "enrich",
-        h2("Widgets tab content")
-      ),
-      # four tab content
-      tabItem(tabName = "idr",
-        h2("Irreproducible Discovery Rate (IDR) Analyses"),
-         fluidRow(
-        box(fileInput("atac_file1_idr", "Upload Replicate 1 Peaks File (.narrowPeaks)"),
-        fileInput("atac_file2_idr", "Upload Replicate 2 Peaks File (.narrowPeaks)"),
-        actionButton("generate_button_idr", "Generate Figure"),
-        actionButton("clear_button_idr", "Clear All"),
-        br(),
-        br(),
-
-        h4(shinycssloaders::withSpinner(htmlOutput('idr_para_int'), color = "grey")),
-        h4(textOutput('idr_para')),
-        h4(htmlOutput('idr_value'))
-        
-        ,height = 500),
-
-        tabBox(
-          side = "right", height = 500,
-          selected = "Rank",
-          tabPanel("Replicate 2",
-          plotOutput('idr_plot_r2')),
-          tabPanel("Repliacte 1",
-          plotOutput('idr_plot_r1')),
-          tabPanel("Signal",
-          shinycssloaders::withSpinner(plotlyOutput('idr_plot_sig'), color = "grey")),
-          tabPanel("Rank",
-            shinycssloaders::withSpinner(plotlyOutput('idr_plot_rank'), color = "grey"))
-          ),
+    ),
+    # main body
+    dashboardBody(
+      tabItems(
+        # home tab content
+        tabItem(
+          tabName = "home",
+          h2("Widgets tab content")
+        ),
+        # First tab content
+        tabItem(
+          tabName = "geneexpress",
+          h2(" Correlation analyses of ATAC-seqs and RNA-seqs"),
           fluidRow(
-         column(width=12,
-         box( 
-          h4("IDR table"),
-          DTOutput('idr_table')
-          ,width = 12))
+            box(fileInput("gene_file", "Upload Gene Expression File (TXT format)"),
+              fileInput("atac_file", "Upload ATAC-seq Abundance File (TXT format)"),
+              radioButtons("plot_type", "Choose Plot Type:",
+                choices = list("Scatter Plot" = "scatter", "Ranked List" = "ranked"),
+                selected = "scatter"
+              ),
+              actionButton("generate_button", "Generate Figure"),
+              actionButton("clear_button", "Clear All"),
+              height = 500
+            ),
+            box(shinycssloaders::withSpinner(plotlyOutput("scatter_plot"), color = "grey"), height = 500)
+          )
+        ),
+        # Second tab content
+        tabItem(
+          tabName = "dep",
+          h2("Widgets tab content")
+        ),
+        # Thirf tab content
+        tabItem(
+          tabName = "enrich",
+          h2("Widgets tab content")
+        ),
+        # four tab content
+        tabItem(
+          tabName = "idr",
+          h2("Irreproducible Discovery Rate (IDR) Analyses"),
+          fluidRow(
+            box(fileInput("atac_file1_idr", "Upload Replicate 1 Peaks File (.narrowPeaks)"),
+              fileInput("atac_file2_idr", "Upload Replicate 2 Peaks File (.narrowPeaks)"),
+              actionButton("generate_button_idr", "Generate Figure"),
+              actionButton("clear_button_idr", "Clear All"),
+              br(),
+              br(),
+              h4(shinycssloaders::withSpinner(htmlOutput("idr_para_int"), color = "grey")),
+              h4(textOutput("idr_para")),
+              h4(htmlOutput("idr_value")),
+              height = 500
+            ),
+            tabBox(
+              side = "right", height = 500,
+              selected = "Rank",
+              tabPanel(
+                "Replicate 2",
+                plotOutput("idr_plot_r2")
+              ),
+              tabPanel(
+                "Repliacte 1",
+                plotOutput("idr_plot_r1")
+              ),
+              tabPanel(
+                "Signal",
+                shinycssloaders::withSpinner(plotlyOutput("idr_plot_sig"), color = "grey")
+              ),
+              tabPanel(
+                "Rank",
+                shinycssloaders::withSpinner(plotlyOutput("idr_plot_rank"), color = "grey")
+              )
+            ),
+            fluidRow(
+              column(
+                width = 12,
+                box(
+                  h4("IDR table"),
+                  
+                  # Input: Choose dataset ----
+                  selectInput("idr_dataset", "Choose a dataset:",
+                    choices = c("IDR", "Replicate 1", "Replicate 2"),
+                 width = 200),
 
-          )))
-
-
-
-
+                  # Button
+                  downloadButton("idr_downloadData", "Download"),
+              br(),
+                            br(),
+                  # table
+                  DTOutput("idr_table"),
+                  width = 12
+                )
+              )
+            )
+          )
+        )
+      )
     )
   )
-)
 )
